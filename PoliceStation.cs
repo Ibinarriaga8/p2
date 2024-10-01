a0f12bd9512f4488a0f49ea0c68ca14aa1f36bbc
Jorge <202206789@alu.comillas.edu>
2024-10-01 14:35:43 +0200

    Creo la clase PoliceStation

---
+++ PoliceStation.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice1
+{
+    public class PoliceStation
+    {
+        //atributos:
+        private bool alert;
+        private List<PoliceCar> policeCars; //lista de coches de policía
+        public PoliceStation()
+        {
+            alert = false;
+            policeCars = new List<PoliceCar>();
+        }
+
+        public void AlertCar(string plate)
+        {
+            alert = true;
+        }
+
+        public void NotifyPolice(Vehicle car)
+        {
+            //Notify each police car in police station
+            foreach (PoliceCar policeCar in policeCars) 
+            { 
+               policeCar.ChaseCar(car);
+            }
+        }
+        public void AddPoliceCar(string plate)
+        {
+            PoliceCar policeCar = new PoliceCar(plate); //registrar coches mediante su matrícula
+            policeCars.Add(policeCar);
+        }
+
+    }
+}
