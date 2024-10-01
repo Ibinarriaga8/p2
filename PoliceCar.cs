111577e071a8e22e76646863b3933d70f28e206f
Jorge <202206789@alu.comillas.edu>
2024-10-01 20:20:59 +0200

    Modifico la clase PoliceCar

--- PoliceCar.cs
+++ PoliceCar.cs
@@ -1,16 +1,20 @@
 ﻿namespace Practice1
 {
-    class PoliceCar : Vehicle
+    public class PoliceCar : Vehicle
     {
         //constant string as TypeOfVehicle wont change allong PoliceCar instances
-        private const string typeOfVehicle = "Police Car"; 
+        private const string typeOfVehicle = "Police Car";
         private bool isPatrolling;
         private SpeedRadar speedRadar;
+        private bool onPursuit;
+        private PoliceStation? policeStation;
+
 
         public PoliceCar(string plate) : base(typeOfVehicle, plate)
         {
             isPatrolling = false;
             speedRadar = new SpeedRadar();
+            onPursuit = false;
         }
 
         public void UseRadar(Vehicle vehicle)
@@ -18,7 +22,25 @@
             if (isPatrolling)
             {
                 speedRadar.TriggerRadar(vehicle);
-                string meassurement = speedRadar.GetLastReading();
+                bool driving_legally = speedRadar.DrivingLegally();
+
+                string meassurement = "";
+                if (driving_legally)
+                {
+                    meassurement = speedRadar.WriteMessage("Driving legally.");
+                }
+                else
+                {
+                    //Vehicle catched above legal speed
+                    meassurement = speedRadar.WriteMessage("Catched above legal speed");
+                    if (policeStation is not null) 
+                    {
+                        string plate = vehicle.GetPlate();  
+                        policeStation.AlertCar(plate); //alerts police station
+                    }
+                    ChaseCar(vehicle); //atempts chasing car if it's currently not on pursuit
+
+                }
                 Console.WriteLine(WriteMessage($"Triggered radar. Result: {meassurement}"));
             }
             else
@@ -58,6 +80,11 @@
             }
         }
 
+        public void SetStation(PoliceStation station)
+        {
+            policeStation = station;
+        }
+
         public void PrintRadarHistory()
         {
             Console.WriteLine(WriteMessage("Report radar speed history:"));
@@ -65,6 +92,20 @@
             {
                 Console.WriteLine(speed);
             }
+        }
+
+        public void ChaseCar(Vehicle vehicle)
+        {
+            if (onPursuit)
+            {
+                Console.WriteLine(WriteMessage("Already on persuit"));
+            }
+            else
+            {
+                onPursuit = true;
+                Console.WriteLine(WriteMessage($"chasing {vehicle}"));
+            }
+
         }
     }
 }
