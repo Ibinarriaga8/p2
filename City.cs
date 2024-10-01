e396d8b9854c87c2e6cba0544723408bb829893d
Jorge <202206789@alu.comillas.edu>
2024-10-01 14:27:16 +0200

    Creo la clase City

---
+++ City.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice1
+{
+    public class City
+    {
+        private PoliceStation comisaria;
+        private List<Taxi> TaxiLicenses;
+
+        //Builder
+        public City()
+        {
+            comisaria = new PoliceStation();
+            TaxiLicenses = new List<Taxi>();
+        }
+
+        public void AddTaxiLicence(string plate)
+        {
+            Taxi taxi = new Taxi(plate);
+            TaxiLicenses.Add(taxi);
+        }
+
+        public void RemoveTaxiLicence(Taxi taxi)
+        {
+            TaxiLicenses.Remove(taxi);
+        }
+        
+
+    }
+
+
+}
